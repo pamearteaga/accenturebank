14e64fd3aca0a81e20a5fcf5c64a1ee5ba557859
IreeRodriguez <ire208@gmail.com>
2018-04-03 20:59:19 -0300

    nuevo componente

---
+++ src/components/accounts/MyAccounts.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyAccounts extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default MyAccounts;
