1bb462effadb13b6db78fcb6106c08a3eee98310
IreeRodriguez <ire208@gmail.com>
2018-04-03 15:09:22 -0300

    login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from '../src/components/navbar/Navbar.js';
+import Login from './components/Login';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar/>
+        <Login/>
       </div>
     );
   }
