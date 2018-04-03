212adc33540ac7f0189c2a15269c63d26dfe5073
pamearteaga <pe_arteaga@hotmail.com>
2018-04-03 20:44:09 -0300

    api

--- src/components/accounts/Accounts.jsx
+++ src/components/accounts/Accounts.jsx
@@ -4,7 +4,32 @@ import '../accounts/Accounts.css';
 const api_url = 'https://api.myjson.com/bins/qmxwd?pretty=1';
 
 class Accounts extends Component {
+
+constructor(props) {
+    super(props);
+    this.state = {
+      cuentas: [],
+    }
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  fetchData(){ // Función que llama a la api
+    fetch(api_url)
+    .then(data => (data.json()))
+    .then(results => {
+      const api_data = results;
+      this.setState({cuentas: api_data})
+    })
+  }
+
+  componentDidMount() { // Cliclo de vida que llama a la función
+    this.fetchData();
+  }
+
+
   render() {
+    const {cuentas} = this.state;
+    console.log({cuentas});
     return (
       <div className="Accounts">
         <select>
@@ -12,10 +37,10 @@ class Accounts extends Component {
           <option value="2">Cuenta 2</option>
           <option value="3">Cuenta 3</option>
         </select>
-        <a id="fecha"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span></a>
+        <a id="fecha"><span className="glyphicon glyphicon-calendar" aria-hidden="true"></span></a>
           <div id="card1">
             <img src="https://www.visa.com.mx/dirija-su-negocio/corporaciones/tarjetas/visa-prepago-agro/_jcr_content/par/cardstack/cardStackColumn2/image.img.jpg/1508184909985.jpg" />
-            <p>Nº de cuenta: 0011-0166-0400656788</p>
+            <p>Nº de cuenta:</p>
             <p>Tipo de cuenta: Cuenta Ahorro</p>
             <h3>Saldo</h3>
             <h1>$3.629</h1>
