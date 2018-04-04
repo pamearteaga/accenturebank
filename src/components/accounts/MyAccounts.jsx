d7496a11501e1ab2550a59a3f6cb844fe98ac919
IreeRodriguez <ire208@gmail.com>
2018-04-03 21:28:11 -0300

    props de myAccount

--- src/components/accounts/MyAccounts.jsx
+++ src/components/accounts/MyAccounts.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class MyAccounts extends Component {
+  constructor(movements, type) {
+    super(movements, type);
+    this.state = {
+      movements, 
+      type
+    };
+  }
   render() {
     return (
       <div>
